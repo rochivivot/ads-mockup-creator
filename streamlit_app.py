d1e429ee233a7919d911f69dba5b1b293bddb8ce
rochivivot <rociovivot@gmail.com>
2025-05-11 11:33:31 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,8 @@ stored_paths = {
     "Sudoku": ("sudoku_sample.jpg", (320, 50), (60, 1220, 520, 78)),
     "Weather_Banner": ("weather_banner_sample.jpg", (320, 50), (60, 1292, 530, 85)),
     "OneFootball": ("onefootball_sample.jpg", (300, 250), (54, 715, 498, 416)),
-    "PLAYit": ("playit_sample.jpg", (300, 250), (60, 600, 300, 250)),
-    "Weather_300x250": ("weather 300x250.jpg", (300, 250), (60, 730, 300, 250)),
+    "PLAYit": ("playit_sample.jpg", (300, 250), (60, 600, 600, 550)),
+    "Weather_300x250": ("weather_300x250_sample.jpg", (300, 250), (60, 730, 300, 250)),
     "Interstitial": ("interstitial_sample.jpg", (320, 480), (48, 180, 320, 480))
 }
 
